257b023aa02ea1690ba332fb2a09f9ea2b06b093
David Lira <dlira@vmware.com>
2023-12-16 15:34:15 -0800

    Added ship and ability to shoot

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var canvas = document.getElementById('canvas');
+var shipCanvas = document.getElementById('ship-canvas');
+var ammoCanvas = document.getElementById('ammo-canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+shipCanvas.width = window.innerWidth;
+shipCanvas.height = window.innerHeight;
+ammoCanvas.width = window.innerWidth;
+ammoCanvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
-
+var c_ship = shipCanvas.getContext('2d');
+var c_ammo = ammoCanvas.getContext('2d');
 class Ball {
     constructor() {
         this.x = Math.floor(Math.random() * canvas.width); //anywhere on x axis
@@ -35,3 +42,51 @@ function drawFrame() {
     window.requestAnimationFrame(drawFrame);
 }
 window.requestAnimationFrame(drawFrame);
+
+class Ship {
+    constructor() {
+        this.animateShip(canvas.width/2, canvas.height/2);
+    }
+
+    animateShip(x, y) {
+        c_ship.clearRect(0, 0, canvas.width, canvas.height);
+        c_ship.beginPath();
+        c_ship.fillStyle = "black";
+        c_ship.arc(x, y, 30, 0, 2 * Math.PI);
+        c_ship.fill()
+    }
+}
+
+var ship = new Ship();
+addEventListener("mousemove", (event) => {
+    window.requestAnimationFrame(() => ship.animateShip(event.clientX, event.clientY));
+});
+
+class Ammo {
+    constructor(x, y) {
+        this.x = x - 5;
+        this.y = y - 60;
+        this.speed = 3;
+    }
+
+    animateAmmo() {
+        if(this.y < 0) return;
+        c_ammo.fillStyle = "#70ed09";
+        c_ammo.fillRect(this.x, this.y, 10, 30)
+        this.y-=this.speed;
+    }
+}
+
+var ammos = [];
+addEventListener("click", (event) => {
+    ammos.push(new Ammo(event.clientX, event.clientY));
+})
+
+function drawAmmo() {
+    c_ammo.clearRect(0, 0, ammoCanvas.width, ammoCanvas.height);
+    ammos.forEach((ammo) => {
+        ammo.animateAmmo();
+    })
+    window.requestAnimationFrame(drawAmmo);
+}
+window.requestAnimationFrame(drawAmmo);
